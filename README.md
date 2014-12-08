ab7594d711e6082708bb6d2abd72fbbf1b8beaf9
Alessandro <ale.settimi@gmail.com>
2014-12-08 22:54:57 +0100

    First commit, just a catkin package ;P

---
+++ README.md
@@ -0,0 +1,1 @@
+# Dual Manipulation Trajectory Planning #
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout<<std::endl;
+    std::cout<<"I'm the dual manipulation trajectory planning!!! . . . implement me!"<<std::endl;
+    std::cout<<std::endl;
+}
