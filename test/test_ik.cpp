3f75901737df90ef5f1fa018ab47049c3c2fc497
hamalMarino <hamal.marino@gmail.com>
2015-03-01 17:43:42 +0100

    Improved test_ik

--- test/test_ik.cpp
+++ test/test_ik.cpp
@@ -3,6 +3,21 @@
 #include <iostream>
 #include "dual_manipulation_shared/ik_service.h"
 
+void check_callback_l(const std_msgs::String::ConstPtr& str)
+{
+    ROS_INFO("Left IK Check : %s",str->data.c_str());
+}
+
+void check_callback_r(const std_msgs::String::ConstPtr& str)
+{
+    ROS_INFO("Right IK Check %s",str->data.c_str());
+}
+
+void check_callback_bimanual(const std_msgs::String::ConstPtr& str)
+{
+    ROS_INFO("Bimanual IK Check : %s",str->data.c_str());
+}
+
 void plan_callback_l(const std_msgs::String::ConstPtr& str)
 {
     ROS_INFO("Left IK Plan : %s",str->data.c_str());
@@ -11,6 +26,11 @@ void plan_callback_l(const std_msgs::String::ConstPtr& str)
 void plan_callback_r(const std_msgs::String::ConstPtr& str)
 {
     ROS_INFO("Right IK Plan : %s",str->data.c_str());
+}
+
+void plan_callback_bimanual(const std_msgs::String::ConstPtr& str)
+{
+    ROS_INFO("Bimanual IK Plan : %s",str->data.c_str());
 }
 
 void exec_callback_l(const std_msgs::String::ConstPtr& str)
@@ -23,6 +43,11 @@ void exec_callback_r(const std_msgs::String::ConstPtr& str)
     ROS_INFO("Right IK Exec : %s",str->data.c_str());
 }
 
+void exec_callback_bimanual(const std_msgs::String::ConstPtr& str)
+{
+    ROS_INFO("Bimanual IK Exec : %s",str->data.c_str());
+}
+
 int main(int argc, char **argv)
 {
     std::cout<<std::endl;
@@ -33,10 +58,15 @@ int main(int argc, char **argv)
     
     ros::NodeHandle n;
     ros::ServiceClient client = n.serviceClient<dual_manipulation_shared::ik_service>("ik_ros_service");
-    ros::Subscriber plan_lsub = n.subscribe("/ik_control/left_hand/planning_done",0,plan_callback_l);
-    ros::Subscriber plan_rsub = n.subscribe("/ik_control/right_hand/planning_done",0,plan_callback_r);
-    ros::Subscriber exec_lsub = n.subscribe("/ik_control/left_hand/action_done",0,exec_callback_l);
-    ros::Subscriber exec_rsub = n.subscribe("/ik_control/right_hand/action_done",0,exec_callback_r);
+    ros::Subscriber check_lsub = n.subscribe("/ik_control/left_hand/check_done",1,check_callback_l);
+    ros::Subscriber check_rsub = n.subscribe("/ik_control/right_hand/check_done",1,check_callback_r);
+    ros::Subscriber check_bimanualsub = n.subscribe("/ik_control/both_hands/check_done",1,check_callback_bimanual);
+    ros::Subscriber plan_lsub = n.subscribe("/ik_control/left_hand/planning_done",1,plan_callback_l);
+    ros::Subscriber plan_rsub = n.subscribe("/ik_control/right_hand/planning_done",1,plan_callback_r);
+    ros::Subscriber plan_bimanualsub = n.subscribe("/ik_control/both_hands/planning_done",1,plan_callback_bimanual);
+    ros::Subscriber exec_lsub = n.subscribe("/ik_control/left_hand/action_done",1,exec_callback_l);
+    ros::Subscriber exec_rsub = n.subscribe("/ik_control/right_hand/action_done",1,exec_callback_r);
+    ros::Subscriber exec_bimanualsub = n.subscribe("/ik_control/both_hands/action_done",1,exec_callback_bimanual);
     dual_manipulation_shared::ik_service srv;
     
     geometry_msgs::Pose ee_pose;
@@ -146,7 +176,7 @@ int main(int argc, char **argv)
 	return 1;
     }
 
-    ros::spinOnce();
+    ros::spin();
 
     return 0;
 }
