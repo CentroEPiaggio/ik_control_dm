26874196142dcc8cb4d6a9e343af8e7f7c23f7b5
hamalMarino <hamal.marino@gmail.com>
2015-04-03 12:49:29 +0200

    Changed test_ik_check_capability to test bimanual IK check and collision with objects (while waiting for input, put an object in the environment from outside, e.g. using test_scene_objects)

--- test/test_ik_check_capability.cpp
+++ test/test_ik_check_capability.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include "dual_manipulation_ik_control_ik_check/ik_check_capability.h"
 
+#define BIMANUAL 1
+
 int main(int argc, char **argv)
 {
     std::cout<<std::endl;
@@ -17,6 +19,14 @@ int main(int argc, char **argv)
     moveit_msgs::DisplayRobotState display_rs_msg;
     
     dual_manipulation::ik_control::ikCheckCapability ik_check_capability;
+    
+    char y;
+    std::cout << "Press any key to continue... ";
+    std::cin >> y;
+    
+//     for(int i=0; i<3; i++)
+      ros::spinOnce();
+    
     // load the robot model
     robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
     moveit::core::RobotModelPtr kinematic_model_ = robot_model_loader.getModel();
@@ -36,27 +46,22 @@ int main(int argc, char **argv)
     sleep(1);
     
     std::vector<geometry_msgs::Pose> ee_poses;
-    ee_poses.resize(1);
-    geometry_msgs::Pose& ee_pose = ee_poses.at(0);
+    geometry_msgs::Pose ee_pose;
     
-    // obtained via direct kinematics: left_hand_palm_link
-    // pos [x y z]: 8.55678e-08 1.1292 1.06425
-    // orient [x y z w]: -0.433013 0.25 0.433013 0.75
-//     ee_pose.position.x = 0.2;
-//     ee_pose.position.y = -0.2+1.1292;
-//     ee_pose.position.z = -0.2+1.06425;
     ee_pose.position.x = -0.4;
-    ee_pose.position.y = +0.0;
+    ee_pose.position.y = -0.1+0.1;
     ee_pose.position.z = +0.5;
-    ee_pose.orientation.x = 0.0; //-0.433013;
-    ee_pose.orientation.y = 0.0; //0.25;
-    ee_pose.orientation.z = 0.707; //0.433013;
-    ee_pose.orientation.w = -0.707; //0.75;
+    ee_pose.orientation.x = 0.0;
+    ee_pose.orientation.y = 0.0;
+    ee_pose.orientation.z = 0.707;
+    ee_pose.orientation.w = 0.707;
+    
+    ee_poses.push_back(ee_pose);
 
-    std::string ee_name = "right_hand";
+    std::string ee_name = "left_hand";
     std::vector<std::vector<double>> solutions;
-    solutions.resize(1);
 
+#ifndef BIMANUAL
     if(!ik_check_capability.find_group_ik(ee_name,ee_poses,solutions))
       ROS_ERROR_STREAM("Unable to solve ik request for " << ee_name << " in pose\n" << ee_pose);
     else
@@ -73,16 +78,14 @@ int main(int argc, char **argv)
 
       sleep(1);
     }
+    
+    ee_pose.position.y = +0.1;
+    ee_pose.orientation.w = -0.707;
+    ee_poses.clear();
+    ee_poses.push_back(ee_pose);
 
-//     ee_pose.position.x = -0.2;
-//     ee_pose.position.y = 0.2-1.1292;
-//     ee_pose.position.z = -0.2+1.06425;
-    ee_pose.position.y = -0.0;
-    ee_pose.orientation.w = 0.707;
-
-    ee_name = "left_hand";
-    solutions.at(0).clear();
-
+    ee_name = "right_hand";
+    
     if(!ik_check_capability.find_group_ik(ee_name,ee_poses,solutions))
       ROS_ERROR_STREAM("Unable to solve ik request for " << ee_name << " in pose\n" << ee_pose);
     else
@@ -97,6 +100,32 @@ int main(int argc, char **argv)
 
       display_publisher.publish(display_rs_msg);
     }
+#else
+    ee_pose.position.y = +0.1-0.1;
+    ee_pose.orientation.w = -0.707;
+    ee_poses.push_back(ee_pose);
+    ee_name = "both_hands";
+
+    if(!ik_check_capability.find_group_ik(ee_name,ee_poses,solutions))
+      ROS_ERROR_STREAM("Unable to solve ik request for " << ee_name << " in pose (1st one only shown here)\n" << ee_poses.at(0));
+    else
+    {
+      moveit::core::JointModelGroup* jmg = kinematic_model_->getJointModelGroup("dual_hand_arm");
+      const std::vector <std::string> ee_names = jmg->getAttachedEndEffectorNames();
+      for(int i=0; i<ee_names.size(); i++)
+      {
+	moveit::core::JointModelGroup* jm_subg = kinematic_model_->getEndEffector(ee_names.at(i));
+	kinematic_state_->setJointGroupPositions(jm_subg,solutions.at(i));
+      }
+
+      //visualize the result
+      display_rs_msg.state.joint_state.position.clear();
+      for(int i=0; i<display_rs_msg.state.joint_state.name.size(); i++)
+	display_rs_msg.state.joint_state.position.push_back(kinematic_state_->getVariablePosition(i));
+
+      display_publisher.publish(display_rs_msg);
+    }
+#endif
 
     ros::spin();
 
