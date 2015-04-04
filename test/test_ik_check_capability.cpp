deaa0d4dd188463d6f12d4883f012605af7a742e
hamalMarino <hamal.marino@gmail.com>
2015-04-04 18:51:23 +0200

    improved a little the test utility

--- test/test_ik_check_capability.cpp
+++ test/test_ik_check_capability.cpp
@@ -22,10 +22,22 @@ int main(int argc, char **argv)
     // load the robot model
     robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
     moveit::core::RobotModelPtr kinematic_model_ = robot_model_loader.getModel();
-    moveit::core::RobotStatePtr kinematic_state_ = robot_state::RobotStatePtr(new robot_state::RobotState(kinematic_model_));
-    kinematic_state_->setToDefaultValues();
+    moveit::core::RobotState kinematic_state_(kinematic_model_);
+    kinematic_state_.setToDefaultValues();
     
+    std::vector<double> initial_guess;
+    bool check_collisions = true;
+    bool return_approximate_solution = true; //false;
+    unsigned int attempts = 0;
+    double timeout = 0.0;
+    std::map <std::string, std::string > allowed_collisions = std::map< std::string, std::string >();
+    allowed_collisions["left_hand_kuka_coupler_bottom"] = "left_arm_5_link";
+    allowed_collisions["right_hand_kuka_coupler_bottom"] = "right_arm_5_link";
+
     dual_manipulation::ik_control::ikCheckCapability ik_check_capability(kinematic_model_);
+    ik_check_capability.reset_robot_state("both_hands","dual_hand_arm_home");
+    
+    kinematic_state_ = ik_check_capability.get_robot_state();
     
     char y;
     std::cout << "Press any key to continue... ";
@@ -34,7 +46,7 @@ int main(int argc, char **argv)
     ros::spinOnce();
     
     //visualize the default configuration (just to be sure it's been reset)
-    robot_state::robotStateToRobotStateMsg(*kinematic_state_, display_rs_msg.state);
+    robot_state::robotStateToRobotStateMsg(kinematic_state_, display_rs_msg.state);
     display_publisher.publish(display_rs_msg);
     
     sleep(1);
@@ -60,11 +72,9 @@ int main(int argc, char **argv)
       ROS_ERROR_STREAM("Unable to solve ik request for " << ee_name << " in pose\n" << ee_pose);
     else
     {
-      moveit::core::JointModelGroup* jmg = kinematic_model_->getEndEffector(ee_name);
-      kinematic_state_->setJointGroupPositions(jmg,solutions.at(0));
-
       //visualize the result
-      robot_state::robotStateToRobotStateMsg(*kinematic_state_, display_rs_msg.state);
+      kinematic_state_ = ik_check_capability.get_robot_state();
+      robot_state::robotStateToRobotStateMsg(kinematic_state_, display_rs_msg.state);
       display_publisher.publish(display_rs_msg);
 
       sleep(1);
@@ -81,11 +91,9 @@ int main(int argc, char **argv)
       ROS_ERROR_STREAM("Unable to solve ik request for " << ee_name << " in pose\n" << ee_pose);
     else
     {
-      moveit::core::JointModelGroup* jmg = kinematic_model_->getEndEffector(ee_name);
-      kinematic_state_->setJointGroupPositions(jmg,solutions.at(0));
-
       //visualize the result
-      robot_state::robotStateToRobotStateMsg(*kinematic_state_, display_rs_msg.state);
+      kinematic_state_ = ik_check_capability.get_robot_state();
+      robot_state::robotStateToRobotStateMsg(kinematic_state_, display_rs_msg.state);
       display_publisher.publish(display_rs_msg);
     }
 #else
@@ -94,39 +102,25 @@ int main(int argc, char **argv)
     ee_poses.push_back(ee_pose);
     ee_name = "both_hands";
 
-    if(!ik_check_capability.find_group_ik(ee_name,ee_poses,solutions))
+    if(!ik_check_capability.find_group_ik(ee_name,ee_poses,solutions,initial_guess,check_collisions,false,attempts,timeout,allowed_collisions))
       ROS_ERROR_STREAM("Unable to solve ik request for " << ee_name << " in pose (1st one only shown here)\n" << ee_poses.at(0));
     else
     {
-      moveit::core::JointModelGroup* jmg = kinematic_model_->getJointModelGroup("dual_hand_arm");
-      const std::vector <std::string> ee_names = jmg->getAttachedEndEffectorNames();
-      for(int i=0; i<ee_names.size(); i++)
-      {
-	moveit::core::JointModelGroup* jm_subg = kinematic_model_->getEndEffector(ee_names.at(i));
-	kinematic_state_->setJointGroupPositions(jm_subg,solutions.at(i));
-      }
-
       //visualize the result
-      robot_state::robotStateToRobotStateMsg(*kinematic_state_, display_rs_msg.state);
+      kinematic_state_ = ik_check_capability.get_robot_state();
+      robot_state::robotStateToRobotStateMsg(kinematic_state_, display_rs_msg.state);
       display_publisher.publish(display_rs_msg);
       
       sleep(1);
     }
     
-    if(!ik_check_capability.find_group_ik(ee_name,ee_poses,solutions,std::vector<double>(),true,true))
-      ROS_ERROR_STREAM("Unable to solve ik request for " << ee_name << " in pose (1st one only shown here)\n" << ee_poses.at(0));
+    if(!ik_check_capability.find_group_ik(ee_name,ee_poses,solutions,initial_guess,check_collisions,return_approximate_solution,attempts,timeout,allowed_collisions))
+      ROS_ERROR_STREAM("Unable to solve approximate ik request for " << ee_name << " in pose (1st one only shown here)\n" << ee_poses.at(0));
     else
     {
-      moveit::core::JointModelGroup* jmg = kinematic_model_->getJointModelGroup("dual_hand_arm");
-      const std::vector <std::string> ee_names = jmg->getAttachedEndEffectorNames();
-      for(int i=0; i<ee_names.size(); i++)
-      {
-	moveit::core::JointModelGroup* jm_subg = kinematic_model_->getEndEffector(ee_names.at(i));
-	kinematic_state_->setJointGroupPositions(jm_subg,solutions.at(i));
-      }
-
       //visualize the result
-      robot_state::robotStateToRobotStateMsg(*kinematic_state_, display_rs_msg.state);
+      kinematic_state_ = ik_check_capability.get_robot_state();
+      robot_state::robotStateToRobotStateMsg(kinematic_state_, display_rs_msg.state);
       display_publisher.publish(display_rs_msg);
       
       sleep(1);
@@ -135,24 +129,16 @@ int main(int argc, char **argv)
     moveit::core::JointModelGroup* jmg = kinematic_model_->getJointModelGroup("dual_hand_arm");
     const std::vector <std::string> ee_names = jmg->getAttachedEndEffectorNames();
     std::string pos_name("dual_hand_arm_home");
-    kinematic_state_->setToDefaultValues(jmg,pos_name);
-    std::vector<double> initial_guess;
-    kinematic_state_->copyJointGroupPositions(jmg,initial_guess);
-    kinematic_state_->setToDefaultValues();
+    kinematic_state_.setToDefaultValues(jmg,pos_name);
+    // kinematic_state_.copyJointGroupPositions(jmg,initial_guess);
+    kinematic_state_.setToDefaultValues();
+    ik_check_capability.reset_robot_state();
     
     std::vector<std::pair<double,std::vector<std::vector<double>>>> it_info;
-    bool result;
-    
     bool store_iterations = true; // false;
+    bool result;
     double allowed_distance = 0.001; //0.5;
     unsigned int trials_nr = 10; // 5;
-    bool check_collisions = true;
-    bool return_approximate_solution = true; //false;
-    unsigned int attempts = 0;
-    double timeout = 0.0;
-    std::map <std::string, std::string > allowed_collisions = std::map< std::string, std::string >();
-    allowed_collisions["left_hand_kuka_coupler_bottom"] = "left_arm_5_link";
-    allowed_collisions["right_hand_kuka_coupler_bottom"] = "right_arm_5_link";
 
     result = ik_check_capability.find_closest_group_ik(ee_name,ee_poses,solutions,it_info,store_iterations,allowed_distance,trials_nr,initial_guess,check_collisions,return_approximate_solution,attempts,timeout,allowed_collisions);
     
@@ -174,14 +160,14 @@ int main(int argc, char **argv)
 	for(int i=0; i<ee_names.size(); i++)
 	{
 	  moveit::core::JointModelGroup* jm_subg = kinematic_model_->getEndEffector(ee_names.at(i));
-	  kinematic_state_->setJointGroupPositions(jm_subg,it.second.at(i));
+	  kinematic_state_.setJointGroupPositions(jm_subg,it.second.at(i));
 	  for(auto& q:it.second.at(i))
 	    std::cout << q << " | ";
 	}
 	std::cout << "|>" << std::endl;
 
 	//visualize the result
-	robot_state::robotStateToRobotStateMsg(*kinematic_state_, display_rs_msg.state);
+	robot_state::robotStateToRobotStateMsg(kinematic_state_, display_rs_msg.state);
 	display_publisher.publish(display_rs_msg);
 	
 	sleep(1);
@@ -191,14 +177,14 @@ int main(int argc, char **argv)
       for(int i=0; i<ee_names.size(); i++)
       {
 	moveit::core::JointModelGroup* jm_subg = kinematic_model_->getEndEffector(ee_names.at(i));
-	kinematic_state_->setJointGroupPositions(jm_subg,solutions.at(i));
+	kinematic_state_.setJointGroupPositions(jm_subg,solutions.at(i));
 	for(auto& q:solutions.at(i))
 	  std::cout << q << " | ";
       }
       std::cout << "|>" << std::endl;
 
       //visualize the result
-      robot_state::robotStateToRobotStateMsg(*kinematic_state_, display_rs_msg.state);
+      robot_state::robotStateToRobotStateMsg(kinematic_state_, display_rs_msg.state);
       display_publisher.publish(display_rs_msg);
 
       sleep(1);
