d55e4835c58583a3db6e6e2dc982f7b0c32d9b38
hamalMarino <hamal.marino@gmail.com>
2015-04-09 17:56:57 +0200

    by default, do not avoid collisions with computeTrajectoryFromWPs

--- include/trajectory_utils.h
+++ include/trajectory_utils.h
@@ -37,7 +37,7 @@ bool splitFullRobotPlan(std::map<std::string,move_group_interface::MoveGroup*> m
   * 
   * @return completed percentage of the trajectory, between 0 and 1; -1 on failure of the time parametrization
   */
-double computeTrajectoryFromWPs(moveit_msgs::RobotTrajectory& trajectory, const std::vector <geometry_msgs::Pose >& waypoints, moveit::planning_interface::MoveGroup* moveGroup, bool avoid_collisions = true, double eef_step = 1.0, double jump_threshold = 0.0);
+double computeTrajectoryFromWPs(moveit_msgs::RobotTrajectory& trajectory, const std::vector <geometry_msgs::Pose >& waypoints, moveit::planning_interface::MoveGroup* moveGroup, bool avoid_collisions = false, double eef_step = 1.0, double jump_threshold = 0.0);
 
 /**
   * @brief utility function to associate hand timing to the robot timing
