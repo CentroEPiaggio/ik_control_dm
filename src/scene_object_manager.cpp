1d5412f0e075a48c0f0c1221edac54f53de28d01
Hamal Marino <hamal.marino@gmail.com>
2016-09-09 14:56:12 +0200

    code auto-linting

--- src/scene_object_manager.cpp
+++ src/scene_object_manager.cpp
@@ -24,7 +24,7 @@ SceneObjectManager::SceneObjectManager()
     // check if the object DB is loaded correctly
     std::cout << "Object DB:" << std::endl;
     for(auto item:db_mapper_->Objects)
-      std::cout << " - " << item.first << ": " << std::get<0>(item.second) << " + " << std::get<1>(item.second) << std::endl;
+        std::cout << " - " << item.first << ": " << std::get<0>(item.second) << " + " << std::get<1>(item.second) << std::endl;
     
     initializeSceneObjects();
 }
@@ -45,158 +45,158 @@ void SceneObjectManager::initializeSceneObjects()
         ROS_WARN_STREAM_NAMED(CLASS_LOGNAME,CLASS_NAMESPACE << __func__ << " : unable to call " << node.resolveName("get_planning_scene",true) << " - starting with an empty planning scene...");
     else
     {
-      for(auto attObject:srv.response.scene.robot_state.attached_collision_objects)
-      {
-	grasped_objects_map_[attObject.object.id] = attObject;
-	#if DEBUG
-    ROS_INFO_STREAM_NAMED(CLASS_LOGNAME,CLASS_NAMESPACE << __func__ << " : added attached collision object " << attObject.object.id);
-	#endif
-      }
-      for(auto object:srv.response.scene.world.collision_objects)
-      {
-	moveit_msgs::AttachedCollisionObject attObject;
-	attObject.object = object;
-	world_objects_map_[object.id] = attObject;
-	#if DEBUG
-    ROS_INFO_STREAM_NAMED(CLASS_LOGNAME,CLASS_NAMESPACE << __func__ << " : added collision object " << attObject.object.id);
-	#endif
-      }
-      ROS_DEBUG_STREAM_NAMED(CLASS_LOGNAME,CLASS_NAMESPACE << __func__ << " : " << node.resolveName("get_planning_scene",true) << " returned \n" << srv.response.scene);
+        for(auto attObject:srv.response.scene.robot_state.attached_collision_objects)
+        {
+            grasped_objects_map_[attObject.object.id] = attObject;
+            #if DEBUG
+            ROS_INFO_STREAM_NAMED(CLASS_LOGNAME,CLASS_NAMESPACE << __func__ << " : added attached collision object " << attObject.object.id);
+            #endif
+        }
+        for(auto object:srv.response.scene.world.collision_objects)
+        {
+            moveit_msgs::AttachedCollisionObject attObject;
+            attObject.object = object;
+            world_objects_map_[object.id] = attObject;
+            #if DEBUG
+            ROS_INFO_STREAM_NAMED(CLASS_LOGNAME,CLASS_NAMESPACE << __func__ << " : added collision object " << attObject.object.id);
+            #endif
+        }
+        ROS_DEBUG_STREAM_NAMED(CLASS_LOGNAME,CLASS_NAMESPACE << __func__ << " : " << node.resolveName("get_planning_scene",true) << " returned \n" << srv.response.scene);
     }
 }
 
 bool SceneObjectManager::manage_object(dual_manipulation_shared::scene_object_service::Request& req)
 {
-  if (req.command == "add")
-  {
-    return addObject(req);
-  }
-  else if (req.command == "remove")
-  {
-    return removeObject(req.object_id);
-  }
-  else if (req.command == "attach")
-  {
-    return attachObject(req);
-  }
-  else if (req.command == "detach")
-  {
-    return addObject(req);
-  }
-  else if (req.command == "remove_all")
-  {
-    return removeAllObjects();
-  }
-  else
-  {
-    ROS_ERROR_STREAM_NAMED(CLASS_LOGNAME,CLASS_NAMESPACE << __func__ << " : Unknown command " << req.command << ", returning");
-    return false;
-  }
+    if (req.command == "add")
+    {
+        return addObject(req);
+    }
+    else if (req.command == "remove")
+    {
+        return removeObject(req.object_id);
+    }
+    else if (req.command == "attach")
+    {
+        return attachObject(req);
+    }
+    else if (req.command == "detach")
+    {
+        return addObject(req);
+    }
+    else if (req.command == "remove_all")
+    {
+        return removeAllObjects();
+    }
+    else
+    {
+        ROS_ERROR_STREAM_NAMED(CLASS_LOGNAME,CLASS_NAMESPACE << __func__ << " : Unknown command " << req.command << ", returning");
+        return false;
+    }
 }
 
 void SceneObjectManager::loadAndAttachMesh(dual_manipulation_shared::scene_object_service::Request& req)
 {
-  // load the appropriate mesh and add it to the CollisionObject
-  moveit_msgs::AttachedCollisionObject& attObject = req.attObject;
-  
-  // NOTE: the mesh should be in ASCII format, in meters, and the frame of reference should be coherent with the external information we get (obviously...)
-  shapes::Mesh* m;
-  shape_msgs::Mesh co_mesh;
-  shapes::ShapeMsg co_mesh_msg;
-  
-  m = shapes::createMeshFromResource(std::get<1>(db_mapper_->Objects.at( (int)req.object_db_id )));
-  m->scale(1); // change this to 0.001 if expressed in mm; this does not change the frame, thus if it's not baricentric the object will be moved around
-  shapes::constructMsgFromShape(m,co_mesh_msg);
-  co_mesh = boost::get<shape_msgs::Mesh>(co_mesh_msg);
-  
-  req.attObject.object.meshes.clear();
-  req.attObject.object.meshes.push_back(co_mesh);
+    // load the appropriate mesh and add it to the CollisionObject
+    moveit_msgs::AttachedCollisionObject& attObject = req.attObject;
+    
+    // NOTE: the mesh should be in ASCII format, in meters, and the frame of reference should be coherent with the external information we get (obviously...)
+    shapes::Mesh* m;
+    shape_msgs::Mesh co_mesh;
+    shapes::ShapeMsg co_mesh_msg;
+    
+    m = shapes::createMeshFromResource(std::get<1>(db_mapper_->Objects.at( (int)req.object_db_id )));
+    m->scale(1); // change this to 0.001 if expressed in mm; this does not change the frame, thus if it's not baricentric the object will be moved around
+    shapes::constructMsgFromShape(m,co_mesh_msg);
+    co_mesh = boost::get<shape_msgs::Mesh>(co_mesh_msg);
+    
+    req.attObject.object.meshes.clear();
+    req.attObject.object.meshes.push_back(co_mesh);
 }
 
 bool SceneObjectManager::addObject(dual_manipulation_shared::scene_object_service::Request req)
 {
-  req.attObject.object.operation = req.attObject.object.ADD;
-
-  moveit_msgs::AttachedCollisionObject& attObject = req.attObject;
-  
-  loadAndAttachMesh(req);
-  
-  ROS_INFO_STREAM("SceneObjectManager::addObject : Putting the object " << attObject.object.id << " into the environment (" << req.attObject.object.header.frame_id << ")...");
-
-  if ((req.command == "add") && (grasped_objects_map_.count(attObject.object.id)))
-  {
-      ROS_WARN_STREAM_NAMED(CLASS_LOGNAME,CLASS_NAMESPACE << __func__ << " : The object was already attached to " << grasped_objects_map_.at(attObject.object.id).link_name << ": moving it to the environment");
-    removeObject(attObject.object.id);
-  }
-  else if (req.command == "detach")
-  {
-// // // //     return true;
+    req.attObject.object.operation = req.attObject.object.ADD;
     
-    if (grasped_objects_map_.count(req.object_id))
+    moveit_msgs::AttachedCollisionObject& attObject = req.attObject;
+    
+    loadAndAttachMesh(req);
+    
+    ROS_INFO_STREAM("SceneObjectManager::addObject : Putting the object " << attObject.object.id << " into the environment (" << req.attObject.object.header.frame_id << ")...");
+    
+    if ((req.command == "add") && (grasped_objects_map_.count(attObject.object.id)))
     {
-      attObject = grasped_objects_map_.at(req.object_id);
-      removeObject(attObject.object.id);
+        ROS_WARN_STREAM_NAMED(CLASS_LOGNAME,CLASS_NAMESPACE << __func__ << " : The object was already attached to " << grasped_objects_map_.at(attObject.object.id).link_name << ": moving it to the environment");
+        removeObject(attObject.object.id);
     }
-    else
+    else if (req.command == "detach")
     {
-        ROS_WARN_STREAM_NAMED(CLASS_LOGNAME,CLASS_NAMESPACE << __func__ << " : The object to detach (" << req.attObject.object.id << ") was not attached to any end-effector: did you grasp it before?");
-      return false;
+        // // // //     return true;
+        
+        if (grasped_objects_map_.count(req.object_id))
+        {
+            attObject = grasped_objects_map_.at(req.object_id);
+            removeObject(attObject.object.id);
+        }
+        else
+        {
+            ROS_WARN_STREAM_NAMED(CLASS_LOGNAME,CLASS_NAMESPACE << __func__ << " : The object to detach (" << req.attObject.object.id << ") was not attached to any end-effector: did you grasp it before?");
+            return false;
+        }
     }
-  }
-  
-  collision_object_publisher_.publish(attObject.object);
-  
-  // store information about this object in a class map
-  world_objects_map_[attObject.object.id] = attObject;
-  
-  return true;
+    
+    collision_object_publisher_.publish(attObject.object);
+    
+    // store information about this object in a class map
+    world_objects_map_[attObject.object.id] = attObject;
+    
+    return true;
 }
 
 bool SceneObjectManager::removeObject(std::string& object_id)
 {
-  // remove associated information about this object
-  if (!(world_objects_map_.count(object_id) || grasped_objects_map_.count(object_id) ))
-  {
-      ROS_WARN_STREAM_NAMED(CLASS_LOGNAME,CLASS_NAMESPACE << __func__ << " : The object " << object_id << " was not in the scene!");
-    return false;
-  }
-  else
-  {
-    std::string where;
-    
-    if (world_objects_map_.count(object_id))
+    // remove associated information about this object
+    if (!(world_objects_map_.count(object_id) || grasped_objects_map_.count(object_id) ))
     {
-      where = world_objects_map_.at(object_id).object.header.frame_id;
-      /* Define the message and publish the operation*/
-      moveit_msgs::CollisionObject remove_object;
-      remove_object.id = object_id;
-      remove_object.header.frame_id = world_objects_map_.at(object_id).object.header.frame_id;
-      remove_object.operation = remove_object.REMOVE;
-      collision_object_publisher_.publish(remove_object);
-      
-      // erase the object from the map
-      world_objects_map_.erase( world_objects_map_.find(object_id) );
+        ROS_WARN_STREAM_NAMED(CLASS_LOGNAME,CLASS_NAMESPACE << __func__ << " : The object " << object_id << " was not in the scene!");
+        return false;
     }
-    else if (grasped_objects_map_.count(object_id))
+    else
     {
-      where = grasped_objects_map_.at(object_id).link_name;
-      // NOTE: this does not put the object back in the world
-      moveit_msgs::AttachedCollisionObject detach_object;
-      detach_object.object.id = object_id;
-      detach_object.link_name = grasped_objects_map_.at(object_id).link_name;
-      detach_object.object.operation = detach_object.object.REMOVE;
-      attached_collision_object_publisher_.publish(detach_object);
-      
-      // the object is put in the world by default
-      world_objects_map_[object_id] = grasped_objects_map_.at(object_id);
-      // erase the object from the map
-      grasped_objects_map_.erase( grasped_objects_map_.find(object_id) );
+        std::string where;
+        
+        if (world_objects_map_.count(object_id))
+        {
+            where = world_objects_map_.at(object_id).object.header.frame_id;
+            /* Define the message and publish the operation*/
+            moveit_msgs::CollisionObject remove_object;
+            remove_object.id = object_id;
+            remove_object.header.frame_id = world_objects_map_.at(object_id).object.header.frame_id;
+            remove_object.operation = remove_object.REMOVE;
+            collision_object_publisher_.publish(remove_object);
+            
+            // erase the object from the map
+            world_objects_map_.erase( world_objects_map_.find(object_id) );
+        }
+        else if (grasped_objects_map_.count(object_id))
+        {
+            where = grasped_objects_map_.at(object_id).link_name;
+            // NOTE: this does not put the object back in the world
+            moveit_msgs::AttachedCollisionObject detach_object;
+            detach_object.object.id = object_id;
+            detach_object.link_name = grasped_objects_map_.at(object_id).link_name;
+            detach_object.object.operation = detach_object.object.REMOVE;
+            attached_collision_object_publisher_.publish(detach_object);
+            
+            // the object is put in the world by default
+            world_objects_map_[object_id] = grasped_objects_map_.at(object_id);
+            // erase the object from the map
+            grasped_objects_map_.erase( grasped_objects_map_.find(object_id) );
+        }
+        
+        ROS_INFO_STREAM_NAMED(CLASS_LOGNAME,CLASS_NAMESPACE << __func__ << " : Object " << object_id << " removed from " << where);
     }
     
-    ROS_INFO_STREAM_NAMED(CLASS_LOGNAME,CLASS_NAMESPACE << __func__ << " : Object " << object_id << " removed from " << where);
-  }
-  
-  return true;
+    return true;
 }
 
 bool SceneObjectManager::removeAllObjects()
@@ -217,38 +217,38 @@ bool SceneObjectManager::removeAllObjects()
 
 bool SceneObjectManager::attachObject(dual_manipulation_shared::scene_object_service::Request& req)
 {
-// // //   removeObject(req.object_id);
-// // //   return true;
-  
-  req.attObject.object.operation = req.attObject.object.ADD;
-  
-  moveit_msgs::AttachedCollisionObject& attObject = req.attObject;
-  
-  loadAndAttachMesh(req);
-
-  ROS_INFO_STREAM_NAMED(CLASS_LOGNAME,CLASS_NAMESPACE << __func__ << " : attaching the object " << attObject.object.id << " to " << attObject.link_name);
-  // remove associated information about this object
-  if ((!world_objects_map_.count(attObject.object.id)) && (!grasped_objects_map_.count(attObject.object.id)))
-  {
-      ROS_WARN_STREAM_NAMED(CLASS_LOGNAME,CLASS_NAMESPACE << __func__ << " : The object " << attObject.object.id << " is not in the scene! Attaching it to " << attObject.link_name << " anyway...");
-  }
-  else
-  {
-    // NOTE: to remove the object from where it is currently, if it's grasped, the procedure needs to be called twice!
-    if(grasped_objects_map_.count(attObject.object.id))
+    // // //   removeObject(req.object_id);
+    // // //   return true;
+    
+    req.attObject.object.operation = req.attObject.object.ADD;
+    
+    moveit_msgs::AttachedCollisionObject& attObject = req.attObject;
+    
+    loadAndAttachMesh(req);
+    
+    ROS_INFO_STREAM_NAMED(CLASS_LOGNAME,CLASS_NAMESPACE << __func__ << " : attaching the object " << attObject.object.id << " to " << attObject.link_name);
+    // remove associated information about this object
+    if ((!world_objects_map_.count(attObject.object.id)) && (!grasped_objects_map_.count(attObject.object.id)))
     {
+        ROS_WARN_STREAM_NAMED(CLASS_LOGNAME,CLASS_NAMESPACE << __func__ << " : The object " << attObject.object.id << " is not in the scene! Attaching it to " << attObject.link_name << " anyway...");
+    }
+    else
+    {
+        // NOTE: to remove the object from where it is currently, if it's grasped, the procedure needs to be called twice!
+        if(grasped_objects_map_.count(attObject.object.id))
+        {
+            removeObject(attObject.object.id);
+            usleep(3000);
+        }
         removeObject(attObject.object.id);
         usleep(3000);
     }
-    removeObject(attObject.object.id);
-    usleep(3000);
-  }
-  
-  // attach it to the robot
-  attached_collision_object_publisher_.publish(attObject);
-  
-  // store information about this object in a class map
-  grasped_objects_map_[attObject.object.id] = attObject;
-  
-  return true;
+    
+    // attach it to the robot
+    attached_collision_object_publisher_.publish(attObject);
+    
+    // store information about this object in a class map
+    grasped_objects_map_[attObject.object.id] = attObject;
+    
+    return true;
 }
