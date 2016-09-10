367a25c3bfcdef8f3de2e01fb1fb0004f93b514a
Hamal Marino <hamal.marino@gmail.com>
2016-09-10 12:54:39 +0200

    shared_ik_memory is now its own separate class

--- include/abstract_capability.h
+++ include/abstract_capability.h
@@ -1,59 +1,15 @@
 #ifndef ABSTRACT_CAPABILITY_H_
 #define ABSTRACT_CAPABILITY_H_
 
-#include <mutex>
-
 #include <dual_manipulation_shared/ik_control_capabilities.h>
 #include <dual_manipulation_shared/ik_service.h>
 #include <dual_manipulation_shared/ik_response.h>
-#include <dual_manipulation_ik_control/group_structure_manager.h>
-#include <dual_manipulation_ik_control/robot_controller_interface.h>
-#include <dual_manipulation_ik_control/robot_state_manager.h>
-#include "scene_object_manager.h"
-#include <moveit/planning_pipeline/planning_pipeline.h>
-#include <moveit/planning_interface/planning_interface.h>
-#include <moveit/move_group_interface/move_group.h>
-#include <XmlRpcValue.h>
+#include "shared_ik_memory.h"
 
 namespace dual_manipulation
 {
 namespace ik_control
 {
-
-/**
- * @brief A structure to share resources across implemented capabilities: plan, move, grasp, ...
- */
-class shared_ik_memory
-{
-public:
-    std::mutex m;
-    XmlRpc::XmlRpcValue* ik_control_params;
-    // share the robot state to use for next planning
-    std::mutex robotState_mutex_;
-    moveit::core::RobotStatePtr planning_init_rs_;
-    // share the planning scene among planning capabilities
-    std::mutex planningScene_mutex_;
-    planning_scene::PlanningSceneConstPtr planning_scene_;
-    // share the motion plans among planning/control capabilities
-    std::mutex movePlans_mutex_;
-    std::map<std::string,moveit::planning_interface::MoveGroup::Plan> movePlans_;
-    // share objects which have to be in the scene
-    std::mutex map_mutex_;
-    std::map<std::string,std::string> grasped_obj_map_;
-    std::map<std::string,moveit_msgs::AttachedCollisionObject> objects_map_;
-    // trajectory execution expected end-time
-    std::mutex end_time_mutex_;
-    ros::Time movement_end_time_;
-    // manage robot group structure
-    std::unique_ptr<const GroupStructureManager> groupManager;
-    // manage robot controllers
-    std::unique_ptr<const RobotControllerInterface> robotController;
-    // manage robot states
-    std::unique_ptr<const RobotStateManager> robotStateManager;
-    // managing the objects in the scene
-    std::unique_ptr<SceneObjectManager> sceneObjectManager;
-};
-
 /**
  * @brief An abstract class implementing a generic capability for use inside ik_control: plan, move, grasp, ...
  */
---
+++ src/shared_ik_memory.cpp
@@ -0,0 +1,8 @@
+#include "shared_ik_memory.h"
+
+using namespace dual_manipulation::ik_control;
+
+shared_ik_memory::shared_ik_memory()
+{
+    
+}
