b4adf8df97ef8a3d67e39ccd2e481e9034b1a649
Hamal Marino <hamal.marino@gmail.com>
2016-11-26 16:49:59 +0100

    removed no longer used functions from trajectory_utils

--- include/trajectory_utils.h
+++ include/trajectory_utils.h
@@ -3,30 +3,8 @@
 
 #include "ros/ros.h"
 #include <dual_manipulation_shared/ik_service.h>
-#include <moveit/move_group_interface/move_group.h>
 #include <moveit_msgs/RobotTrajectory.h>
 #include <moveit/robot_trajectory/robot_trajectory.h>
-#include "ik_check_capability/ik_check_capability.h"
-
-/**
-  * @brief utility function to convert a waypoint sequence into a robot trajectory
-  * 
-  * @param trajectory
-  *   contains the returned moveit_msgs::RobotTrajectory if everything worked
-  * @param waypoints
-  *   points to follow
-  * @param ikCheck
-  *   an ikCheckCapability instance which can operate on the group we want to compute WPs for
-  * @param group_name
-  *   the name of the group we want to operate on (as defined in the SRDF)
-  * @param ee_name
-  *   the name of the group we want to operate on (as defined in ikCheckCapability)
-  * @param avoid_collisions
-  *   default: false - says whether to fail if a collision is found
-  * 
-  * @return completed percentage of the trajectory, between 0 and 1; -1 on failure of the time parametrization
-  */
-double computeTrajectoryFromWPs(moveit_msgs::RobotTrajectory& trajectory, const std::vector <geometry_msgs::Pose >& waypoints, dual_manipulation::ik_control::ikCheckCapability& ikCheck, std::string group_name, std::string ee_name, bool avoid_collisions, double allowed_distance, std::vector<double>& single_distances, uint trials_nr = 10, uint attempts_nr = 0);
 
 /**
   * @brief utility function to associate hand timing to the robot timing
@@ -35,15 +13,6 @@ double computeTrajectoryFromWPs(moveit_msgs::RobotTrajectory& trajectory, const 
   * TODO: implement this last part
   */
 bool computeHandTiming(moveit_msgs::RobotTrajectory& trajectory,dual_manipulation_shared::ik_service::Request& req);
-
-/**
-  * @brief utility to add a waypoint to a trajectory message using a robot state
-  * 
-  * @param rs the robot state representing the waypoint
-  * @param group_name the name of the group to which the trajectory is associated
-  * @param traj robot trajectory message to which a waypoint is added
-  */
-bool add_wp_to_traj(const moveit::core::RobotStatePtr& rs, std::string group_name, moveit_msgs::RobotTrajectory& traj);
 
 /**
   * @brief utility to append a trajectory message to another (for the same group)
--- src/trajectory_utils.cpp
+++ src/trajectory_utils.cpp
@@ -1,59 +1,7 @@
 #include "trajectory_utils.h"
 #include <moveit/trajectory_processing/iterative_time_parameterization.h>
-#include "ik_check_capability/ik_check_capability.h"
 
 #define CLASS_NAMESPACE "trajectory_utils::"
-
-double computeTrajectoryFromWPs(moveit_msgs::RobotTrajectory& trajectory, const std::vector< geometry_msgs::Pose >& waypoints, dual_manipulation::ik_control::ikCheckCapability& ikCheck, std::string group_name, std::string ee_name, bool avoid_collisions, double allowed_distance, std::vector< double >& single_distances, uint trials_nr, uint attempts_nr)
-{
-  // compute waypoints
-  double completed = 0.0;
-  int i;
-  std::vector<double> solution;
-  std::vector<double> initial_guess;
-  
-  // store starting robot state and add it to the trajectory
-  moveit::core::RobotStatePtr init_rs(new moveit::core::RobotState(ikCheck.get_robot_state()));
-  add_wp_to_traj(init_rs,group_name,trajectory);
-  
-  bool return_approximate_solution = false;
-  
-  for(i=0; i<waypoints.size(); i++)
-  {
-    if(!ikCheck.find_closest_group_ik(ee_name,waypoints.at(i),solution,allowed_distance,single_distances,trials_nr,initial_guess,avoid_collisions,return_approximate_solution, attempts_nr))
-      break;
-    
-    moveit::core::RobotStatePtr rs(new moveit::core::RobotState(ikCheck.get_robot_state()));
-    
-    if(!add_wp_to_traj(rs,group_name,trajectory))
-      break;
-  }
-  completed = (double)i/waypoints.size();
-
-    // show an error if I didn't complete the trajectory, but continue with the parametrization (unless I didn't find even a single waypoint...)
-    if(i < 1)
-    {
-        ROS_ERROR_STREAM(CLASS_NAMESPACE << __func__ << " : error in computing trajectory >> NO waypoint was converted!");
-    }
-    if(completed < 1.0)
-        ROS_WARN_STREAM(CLASS_NAMESPACE << __func__ << " : error in computing trajectory >> completed part = " << completed*100.0 << "%");
-  
-  // interpolate them
-  //NOTE: robot_traj, built on robot_model, contains the full robot; trajectory, instead, is only for the group joints
-  robot_trajectory::RobotTrajectory robot_traj(ikCheck.get_robot_state().getRobotModel(),group_name);
-  robot_traj.setRobotTrajectoryMsg(ikCheck.get_robot_state(),trajectory);
-  trajectory_processing::IterativeParabolicTimeParameterization iptp;
-
-  // compute time stamps
-  if (!iptp.computeTimeStamps(robot_traj))
-  {
-    ROS_ERROR("trajectory_utils::computeTrajectoryFromWPs : unable to compute time stamps for the trajectory");
-    return -1;
-  }
-  robot_traj.getRobotTrajectoryMsg(trajectory);
-
-  return completed;
-}
 
 bool computeHandTiming(moveit_msgs::RobotTrajectory& trajectory, dual_manipulation_shared::ik_service::Request& req)
 {
@@ -75,27 +23,6 @@ bool computeHandTiming(moveit_msgs::RobotTrajectory& trajectory, dual_manipulati
   }
   
   //TODO: impose velocity limits
-}
-
-bool add_wp_to_traj(const moveit::core::RobotStatePtr& rs, std::string group_name, moveit_msgs::RobotTrajectory& traj)
-{
-  //NOTE: robot_traj, built on robot_model, contains the full robot; trajectory, instead, is only for the group joints
-  robot_trajectory::RobotTrajectory robot_traj(rs->getRobotModel(),group_name); // rs->getJointModelGroup(group_name)->getName());
-  if(!traj.joint_trajectory.points.empty())
-    robot_traj.setRobotTrajectoryMsg(*rs,traj);
-  // NOTE: on purpose, very long time interval to be safe in case something goes wrong!
-  robot_traj.addSuffixWayPoint(rs,10.0);
-
-  trajectory_processing::IterativeParabolicTimeParameterization iptp;
-
-  // compute time stamps
-  bool timestamps_ok = iptp.computeTimeStamps(robot_traj);
-  if (!timestamps_ok)
-    ROS_ERROR("trajectory_utils::add_robot_state_to_traj : unable to compute time stamps for the trajectory");
-  else
-    robot_traj.getRobotTrajectoryMsg(traj);
-
-  return timestamps_ok;
 }
 
 bool append_trajectories(const moveit::core::RobotStatePtr& rs, std::string group_name, moveit_msgs::RobotTrajectory& trajA, moveit_msgs::RobotTrajectory& trajB)
